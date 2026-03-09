a8947ccac07901b1e21addc6d307dc53d1d3f36b
colin <294046343@qq.com>
2026-03-09 19:10:14 +0800

    feat: Add Header component and background image to Home page. Introduced Logo and pg1_bg assets, updated Home component to include Header and modified layout styling.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="flex w-full justify-between items-center px-[136px] sticky top-[36px] z-10">
+            <img src="/images/Logo.png" className="w-[184px] h-[48px]" alt="logo" />
+        </header>
+    )
+}
+
+export default Header
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,18 @@
+import Header from '@/components/header/Header'
 import { motion } from 'framer-motion'
 
 function Home() {
   return (
-    <motion.div
-      className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-8"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    >
-      <motion.h1
-        className="text-3xl font-bold text-gray-900 mb-4"
-        initial={{ y: -20 }}
-        animate={{ y: 0 }}
-        transition={{ delay: 0.2 }}
+    <div className='min-h-screen flex flex-col'>
+      <motion.div
+        className="bg-[linear-gradient(rgba(5,5,5,0.48),rgba(5,5,5,0.48)),url('/images/pg1_bg.webp')] bg-cover bg-center aspect-[5760/3840]"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
       >
-        Noah Finance
-      </motion.h1>
-      <motion.p
-        className="text-gray-600"
-        initial={{ y: -10 }}
-        animate={{ y: 0 }}
-        transition={{ delay: 0.3 }}
-      >
-        Vite + React + Tailwind CSS + Framer Motion
-      </motion.p>
-    </motion.div>
+        <Header />
+      </motion.div>
+    </div>
   )
 }
 
