982a7fd14301873d812588e5022f7c8c4f050c04
colin <294046343@qq.com>
2026-03-10 19:39:14 +0800

    feat: Implement NoahButton component and integrate it into Header. Added corresponding CSS for styling the button with a spinning background effect.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,18 @@
+import NoahButton from "@/noahButton/NoahButton"
+
 const Header = () => {
     return (
         <header className="flex w-full justify-between items-center px-[136px] sticky top-[36px] z-10">
             <img src="/images/Logo.png" className="w-[184px] h-[48px]" alt="logo" />
+            <Tabs />
         </header>
+    )
+}
+const Tabs = () => {
+    return (
+        <NoahButton>
+
+        </NoahButton>
     )
 }
 
---
+++ src/noahButton/NoahButton.tsx
@@ -0,0 +1,10 @@
+import styles from "./NoahButton.module.css";
+const NoahButton = () => {
+
+    return (
+        <div className={styles.card}>
+            skdhfskfhdskfhdskjfhsdkjfhskjfhdskjfdskj
+        </div>
+    )
+}
+export default NoahButton;
