1a24879ef831e9d265059a60e58fae12db817cbd
arise <294046343@qq.com>
2026-04-09 00:21:26 +0800

    deploy: github pages static export

--- _next/static/-6fgbnx1iE5KntYLEBdsj/_buildManifest.js
+++ _next/static/-6fgbnx1iE5KntYLEBdsj/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/noahfinance//_next/:path+",
+        "destination": "/noahfinance/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
