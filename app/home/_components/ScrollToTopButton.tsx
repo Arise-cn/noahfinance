214d376120fa0ccff585551bfc68af43b8986266
colin <294046343@qq.com>
2026-04-09 19:30:35 +0800

    feat: Add hover effect to ScrollToTopButton with new image for improved user interaction and visual feedback.

--- app/home/_components/ScrollToTopButton.tsx
+++ app/home/_components/ScrollToTopButton.tsx
@@ -14,10 +14,25 @@ const ScrollToTopButton = () => {
     <button
       type="button"
       onClick={handleScrollToTop}
-      className="fixed bottom-[130px] right-[136px] z-40 flex items-center justify-center rounded-full"
+      className="group fixed bottom-[130px] right-[136px] z-40 flex items-center justify-center rounded-full"
       aria-label="Scroll to top"
     >
-      <Image src="/images/arrow_up.png" alt="arrow up" width={48} height={48} />
+      <span className="relative block h-12 w-12">
+        <Image
+          src="/images/arrow_up.png"
+          alt="arrow up"
+          width={48}
+          height={48}
+          className="absolute inset-0 opacity-100 transition-opacity duration-200 group-hover:opacity-0"
+        />
+        <Image
+          src="/images/arrow_up_hover.png"
+          alt="arrow up hover"
+          width={48}
+          height={48}
+          className="absolute inset-0 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+        />
+      </span>
     </button>
   );
 };
